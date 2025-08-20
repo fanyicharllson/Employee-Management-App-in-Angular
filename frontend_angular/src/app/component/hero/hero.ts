5099289a64ca7cef1d6a856cf8afa816f95ff0ff
fanyicharllson <fanyicharllson1@gmail.com>
2025-08-19 18:00:53 -0700

    Modify the landing page hero section

--- frontend_angular/src/app/component/hero/hero.ts
+++ frontend_angular/src/app/component/hero/hero.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-hero',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './hero.css'
 })
 export class Hero {
+  router = inject(Router);
+  toast = inject(ToastrService);
+
+  navigateToRegiter() {
+    this.router.navigate(['/register']);
+  }
+
+  showSuccessToast() {
+    this.toast.info("Temporarily Unavailable, Please try again later!", "Info")
+  }
 
 }
