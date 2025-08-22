afc60f58f28b0876bf3c5e712cef16e388f2080d
fanyicharllson <fanyicharllson1@gmail.com>
2025-08-21 18:58:20 -0700

    Made changes to the backend api url

--- frontend_angular/src/environments/environment.ts
+++ frontend_angular/src/environments/environment.ts
@@ -1,8 +1,10 @@
+
+// const BACKEND_BASE_URL = 'http://localhost:8080'; // dev mode
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8080/api/v1/user-registration',
-  endapiBaseUrl: 'http://localhost:8080/api/auth',
-  onBoardingApiUrl: 'http://localhost:8080/api/user',
-  employeeApiUrl: 'http://localhost:8080/api/employee',
-  employeeRegisterApiUrl: 'http://localhost:8080/api/auth/employee',
+  apiBaseUrl: 'https://ems-backend-latest-k62u.onrender.com/api/v1/user-registration',
+  endapiBaseUrl: 'https://ems-backend-latest-k62u.onrender.com/api/auth',
+  onBoardingApiUrl: 'https://ems-backend-latest-k62u.onrender.com/api/user',
+  employeeApiUrl: 'https://ems-backend-latest-k62u.onrender.com/api/employee',
+  employeeRegisterApiUrl: 'https://ems-backend-latest-k62u.onrender.com/api/auth/employee',
 };
